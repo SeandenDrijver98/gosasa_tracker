dfb7bd37e5b025e61a701f640b54c121c27c1d20
Sean den Drijver <sddrijver@gmail.com>
2022-01-02 10:43:27 +0200

    only send daily report on weekdays

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate --noinput
 web: gunicorn gosasa_tracker.wsgi --log-file -
+worker: celery -A gosasa_tracker worker -l info
--- gosasa_scraper/management/commands/send_daily_prices.py
+++ gosasa_scraper/management/commands/send_daily_prices.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand
 from Market.tasks import send_daily_prices_mail
-
+from datetime import datetime
 
 class Command(BaseCommand):
     help = 'Send Daily Tracked Product Prices'
 
     def handle(self, *args, **options):
+        weekday = datetime.weekday()
+        if weekday > 4:
+            return
+        
         send_daily_prices_mail()
